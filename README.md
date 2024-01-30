265b7bdec99491e76e359a585a726adba7303f32
mateushr23 <mateushr23@gmail.com>
2024-01-29 23:10:34 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+<h1 align="center"> Desafio Intensivão 02 </h1>
+
+Projeto realizado no curso Explorer da [Rocketseat](https://www.rocketseat.com.br) com o objetivo de estudar manipulação dados em JavaScript.
+
+<p align="center">
+  <img alt="desafio 02" src=".github/preview.png" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- JavaScript
---
+++ script.js
@@ -0,0 +1,80 @@
+// 1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+//alert("Hello World!")
+
+//---------------------------------------------------------------------------------------------------------------------------------
+// 2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+// let a = 2
+// let b = 3
+// let sum = a + b
+
+// alert(`A soma de ${a} com ${b} é ${sum}`)
+
+//---------------------------------------------------------------------------------------------------------------------------------
+// 3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+//     💡 Para saber o tipo de dado você pode usar o operador `typeof`
+// let a = 35
+// if (typeof a == "number") {
+//   alert("É um número")
+// } else {
+//   alert("Não é um número")
+// }
+
+//---------------------------------------------------------------------------------------------------------------------------------
+// 4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+// let a = "dois"
+// if (typeof a == "string") {
+//   alert("É uma string")
+// } else {
+//   alert("Não é uma string")
+// }
+
+//---------------------------------------------------------------------------------------------------------------------------------
+// 5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+// let a = false
+// if (typeof a == "boolean") {
+//   alert("É um booleano")
+// } else {
+//   alert("Não é um booleano")
+// }
+
+//---------------------------------------------------------------------------------------------------------------------------------
+// 6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+// let a = 2
+// let b = 3
+// let sub = a - b
+
+// alert(`A subtração de ${a} com ${b} é ${sub}`)
+
+//---------------------------------------------------------------------------------------------------------------------------------
+// 7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+// let a = 2
+// let b = 3
+// let mult = a * b
+
+// alert(`A multiplicação de ${a} com ${b} é ${mult}`)
+
+//---------------------------------------------------------------------------------------------------------------------------------
+// 8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+// let a = 2
+// let b = 3
+// let div = a / b
+
+// alert(`A divisão de ${a} por ${b} é ${div}`)
+
+//---------------------------------------------------------------------------------------------------------------------------------
+// 9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+// let a = Number(prompt("Entre com um número: "))
+// if (a % 2 == 0) {
+//   alert("É um número par")
+// } else {
+//   alert("Não é um número par")
+// }
+
+//---------------------------------------------------------------------------------------------------------------------------------
+// 10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+// let a = Number(prompt("Entre com um número: "))
+// if (a % 2 == 0) {
+//   alert("Não é um número ímpar")
+// } else {
+//   alert("É um número ímpar")
+// }
